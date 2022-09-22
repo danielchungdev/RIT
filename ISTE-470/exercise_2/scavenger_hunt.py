cc81372b0c969863e05f41c832e2a13a236876b4
pikachungg <pikachungg@gmail.com>
2022-09-22 14:45:16 -0400

    update exercise 2

--- ISTE-470/exercise_2/scavenger_hunt.py
+++ ISTE-470/exercise_2/scavenger_hunt.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+import os
 
 def read_waveforms(LA, RV, RA) :
     infile = open('waveforms.csv', 'r')
@@ -40,13 +42,51 @@ def read_times(TL, TR) :
     data = line.strip().split(',')
 
     for i in range(0, len(data)) : 
-        data[i] = float(data[i])
+        data[i] = float(data[i]) * 1000
 
     TR.append(data)
     infile.close()
 
-def plot_waveforms(LA, RV, RA, TL, TR) :
-    print('create your individual waveform plots here')
+def plot_waveforms(LA, RV, RA, TL, TR):
+    os.makedirs('./waveforms-images', exist_ok=True)
+    for i in range(0, len(LA)):
+        plt.rcParams.update({'font.size': 8})
+
+        plt.subplot( 311 )
+        axes = plt.gca()
+        plt.plot(TL, LA[i, :])
+        axes.tick_params(direction="in", top=True, right=True)
+        plt.yticks(np.arange(0, max(LA[i, :]) + 5, step=5))
+        plt.xticks(np.arange(0, max(TL) + 1, step=5))
+        plt.xlim([0, max(TL) + 1])
+        plt.ylim([0, math.floor(max(LA[i, :]) + 5)])
+        plt.title("Wave forms for Instance {}".format(i + 1))
+        plt.ylabel('Lin Accel (g)')
+
+        plt.subplot( 312 )
+        axes = plt.gca()
+        plt.plot(TR, RV[i, :])
+        axes.tick_params(direction="in", top=True, right=True)
+        plt.yticks(np.arange(0, max(RV[i, :]) + 5, step=5))
+        plt.xticks(np.arange(0, max(TR) + 1, step=5))
+        plt.xlim([0, max(TR) + 1])
+        plt.ylim([0, math.floor(max(RV[i, :]) + 5)])
+        plt.ylabel('Rot Vel (rad/sec)')
+
+        plt.subplot( 313 )
+        axes = plt.gca()
+        plt.plot(TR, RA[i, :])
+        axes.tick_params(direction="in", top=True, right=True)
+        plt.yticks(np.arange(0, max(RA[i, :]) + 2, step=2))
+        plt.xticks(np.arange(0, max(TR) + 1, step=5))
+        plt.xlim([0, max(TR) + 1])
+        plt.ylim([0, math.floor(max(RA[i, :]) + 2)])
+        plt.ylabel('Rot Accel (rad/sec^2)')
+
+        plt.xlabel('Time (ms)')
+
+        plt.savefig('./waveforms-images/Instance ' + str(i + 1) + '.png')
+        plt.close()
     
 # make empty data and time Lists
 LA_list = []
@@ -65,23 +105,6 @@ RA = np.array(RA_list)
 TL = np.array(TL_list[0])
 TR = np.array(TR_list[0])
 
-plt.plot(TL, LA[6, :])
-plt.title('Linear Acceleration of Instance 7')
-plt.xlabel('Time (ms)')
-plt.ylabel('Lin Accel (g)')
-plt.xticks(np.arange(0, 55, step=5))
-plt.savefig('Instance 7.png')
-plt.close()
+plot_waveforms(LA, RV, RA, TL, TR)
 
-
-for i in range(0, 1):
-    plt.subplot( 3, 1, 1 )
-    plt.plot(TL, LA[i, :])
-    plt.title("Wave forms for Instance {}".format(i + 1))
-    plt.subplot( 3, 1, 2 )
-    plt.plot(TR, RV[i, :])
-    plt.subplot( 3, 1, 3 )
-    plt.plot(TL, RA[i, :])
-    plt.savefig('Instance ' + str(i + 1) + '.png')
-    plt.xlabel('Time (ms)')
-    plt.close()
+#Only 5 of them go over 100 in linear acceleration. 
